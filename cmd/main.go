6c730f2bcdcc9bb2de9438f8724c8e31eadcc1e7
Maksym Tretiak <m.tretiak@maytech.net>
2021-11-06 19:14:40 +0200

    Add entrypoint

---
+++ cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/mmtretiak/siden/internal/file/handler"
+	"github.com/mmtretiak/siden/internal/file/service"
+	"github.com/mmtretiak/siden/pkg/logger"
+)
+
+const (
+	loggerType  = logger.DevelopmentLogType
+	bufferSize  = 1024 * 1024 * 500
+	pathToStore = "./store"
+	maxMemory   = 1024 * 1024 * 50
+	addr        = "localhost:8080"
+)
+
+func main() {
+	log, err := logger.New(loggerType)
+	if err != nil {
+		panic(err)
+	}
+
+	router := mux.NewRouter()
+
+	fileService := service.New(bufferSize, pathToStore, log)
+	fileHandler := handler.New(fileService, maxMemory, log)
+
+	router.HandleFunc("/file", fileHandler.Save).Methods(http.MethodPost)
+	router.HandleFunc("/file", fileHandler.GetProcessed).Methods(http.MethodGet)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    addr,
+	}
+
+	log.Infof("listening on %s", addr)
+
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
